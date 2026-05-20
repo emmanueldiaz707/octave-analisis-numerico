36472250e620418e0909865f89140a3168ca97b1
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2026-05-19 21:20:51 -0300

    problema evolutivo viejo

--- calor-barra/viejos/problemaevolutivobarra2.m
+++ calor-barra/viejos/problemaevolutivobarra2.m
@@ -1,18 +1,24 @@
 % Problema de conducción de calor en una barra (evolutivo)
 
 L = 1;          % Longitud de la barra      0<x<L
-T = 0.2;         % Tiempo que transcurre     0<t<T
+T = 0.2;        % Tiempo que transcurre     0<t<T
 
 h = 0.2;        % paso x
 k = 0.02;       % paso t
 
-n = L/h;        % cant puntos x
+% n = L/h;        % cant puntos x
+n = lenght(x) - 2;
 l = T/k;        % cant puntos t 
 
+# Mallado
 x = [0:h:L];  
 t = [0:k:T];     
 
 R = k/(h^2); 
+
+% Condición inicial: u(x,0) = 4x - 4x^2
+u = zeros(h,k);
+
 
 % Vector b
 b = 2.*k*ones(n,1);
@@ -38,4 +44,5 @@ A
 b
 
 
-
+% https://chatgpt.com/c/69fa8297-b01c-8328-8e28-6064dfb078c2
+% https://gemini.google.com/u/0/app/8c1b0ad701071af7?hl=es
