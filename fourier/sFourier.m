021f446a7754099035515b9f03244be5be859963
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2026-06-15 10:02:55 -0300

    agregar comentarios a serie de fourier

--- fourier/sFourier.m
+++ fourier/sFourier.m
@@ -1,13 +1,18 @@
-function y = sFourier(f,x,n)
-  a = -pi;
-  b = pi;
-  k = 1;
-  a0 = (1/pi) * integral(f,a,b);
-  y = (a0/2);
-  f_x = f(x);
-  for k=1:n 
-    an = (1/pi) * integral(@(x) f(x).*cos(k*x),a,b);
-    bn = (1/pi) * integral(@(x) f(x).*sin(k*x),a,b);
-    y = y + an * cos(k*x) + bn * sin(k*x);  
-  endfor
+function y = sFourier(f,x,nmax)
+    % Serie de Fourier con senos y cosenos en [-pi,pi]
+    % (esta implementación no aprovecha la paridad de f)
+    % f:    función integrable con período 2pi
+    % x:    vector con puntos
+    % nmax:    iteraciones
+    % Intervalo: 
+    a = -pi;
+    b = pi;
+  
+    a0 = (1/pi) * integral(f,a,b);
+    y = (a0/2);
+    for n=1:nmax 
+        an = (1/pi) * integral(@(x) f(x).*cos(n*x),a,b);
+        bn = (1/pi) * integral(@(x) f(x).*sin(n*x),a,b);
+        y = y + an * cos(n*x) + bn * sin(n*x);  
+    endfor
 endfunction
